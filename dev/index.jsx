2c3ce4c56358810676cae6df49342451bbf89ae8
jisenber <isenberg.jacob@yahoo.com>
2017-09-17 22:23:03 -0700

    links and nav set up, although architecture is hacky and should be improved

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,23 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import createReactClass from 'create-react-class'
-import {Route, HashRouter, Link } from 'react-router-dom'
+import {Route, HashRouter, NavLink } from 'react-router-dom'
 
-var HelloWorld = createReactClass({
-  render: function() {
-    return (
-      <h1>Hello, {this.props.greetTarget}</h1>
-    )
-  }
-});
-
-var App = createReactClass ({
+var Bio = createReactClass ({
   render: function() {
     return (
       <div>
-        <ul className = "header">
-          <li><Link to="/" activeClassName="active">Home</Link></li>
+        <div>
+          <Nav/>
+        </div>
+        <p className="pushDown"> This is the Bio page</p>
+    </div>
+    )
+  }
+})
+
+var Portfolio = createReactClass({
+  render: function() {
+    return (
+      <div>
+          <Nav/>
+        <p className="pushDown"> This is the Portfolio page</p>
+        </div>
+    )
+  }
+})
+
+var Contact = createReactClass({
+  render: function() {
+    return (
+      <div>
+        <div>
+          <Nav/>
+        </div>
+        <p className="pushDown"> This is the Contact page</p>
+    </div>
+    )
+  }
+})
+
+var Home = createReactClass({
+  render: function() {
+    return (
+      <div>
+        <div>
+          <Nav/>
+        </div>
+        <p className="pushDown"> This is the Home page</p>
+    </div>
+    )
+  }
+})
+
+var Nav = createReactClass ({
+  render: function() {
+    return (
+      <div className="container-fluid">
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <ul id="nav_pills" className="nav nav-pills" role="tablist">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/bio">Bio</NavLink></li>
+          <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
+        </nav>
       </div>
     );
   }
@@ -25,8 +72,12 @@ var App = createReactClass ({
 
 ReactDOM.render(
   <HashRouter>
-    <Route path="/" component={App}>
-    </Route>
+  <div>
+    <Route exact path="/" component={Home}></Route>
+    <Route exact path="/bio" component={Bio}></Route>
+    <Route exact path="/portfolio" component={Portfolio}></Route>
+    <Route exact path="/contact" component={Contact}></Route>
+  </div>
   </HashRouter>,
   document.querySelector('#container')
 );
