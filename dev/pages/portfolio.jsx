ee38ac8bf90579ae38f75f3025d9fa94aac6abb0
jisenber <isenberg.jacob@yahoo.com>
2017-09-20 22:00:56 -0700

    bio page added with some filler and icons and contact removed

--- dev/pages/portfolio.jsx
+++ dev/pages/portfolio.jsx
@@ -52,9 +52,9 @@ var Slider = createReactClass({
   render: function() {
     return (
       <div>
-        { this.state.count === 1 ? <Slide imgSrc="https://i.imgur.com/4260Qwd.png" title="Space Invaders/Parking Hall of Shame" desc="A platform to shame those who park poorly" tools="Node, Express, jQuery, Mongo, Nunjucks" navigate="https://github.com/jkwong5/ParkingHOS" devYear= "2016, 2017 (refactor)"/> : null }
-        { this.state.count === 2 ? <Slide imgSrc="https://i.imgur.com/X6df5yM.png" title="Devolunteer" desc="A platform to connect software developers and non-profits" tools="Mongo, Express, Angular, Node" navigate="https://github.com/devolunteer/devolunteer" devYear="2017"/> : null }
-        { this.state.count === 3 ? <Slide imgSrc="https://i.imgur.com/omgHKYp.png" title="World Capitals Game" desc="A world capitals trivia game (first app I ever built!)" tools="Sweet Vanilla JavaScript" navigate="https://github.com/jisenber/World_Capitals_Game" devYear="2015"/> : null }
+        { this.state.count === 1 ? <Slide imgSrc="https://i.imgur.com/4260Qwd.png" title="Space Invaders/Parking Hall of Shame" desc="A platform to shame those who park poorly" tools="Node, Express, jQuery, Mongo, Nunjucks" github="https://github.com/jkwong5/ParkingHOS" deployed="https://parking-hall-of-shame.herokuapp.com" devYear= "2016, 2017 (refactor)"/> : null }
+        { this.state.count === 2 ? <Slide imgSrc="https://i.imgur.com/X6df5yM.png" title="Devolunteer" desc="A platform to connect software developers and non-profits" tools="Mongo, Express, Angular, Node" github="https://github.com/devolunteer/devolunteer" deployed="https://devolunteer.herokuapp.com" devYear="2017"/> : null }
+        { this.state.count === 3 ? <Slide imgSrc="https://i.imgur.com/omgHKYp.png" title="World Capitals Game" desc="A world capitals trivia game (first app I ever built!)" tools="Sweet Vanilla JavaScript" github="https://github.com/jisenber/World_Capitals_Game" deployed="https://jisenber.github.io/World_Capitals_Game" devYear="2015"/> : null }
         <button className="btn btn-primary" id="counterNext" onClick={this.increase}>NEXT PROJECT</button>
         <button className="btn btn-primary" id="counterPrev" onClick={this.decrease}>PREVIOUS PROJECT</button>
       </div>
@@ -73,13 +73,15 @@ var Slide = createReactClass({
               <tr>
                 <th>Primary Tools</th>
                 <th>Github Repo</th>
-                <th>Year Developed</th>
+                <th>Live Site</th>
+                <th>Year</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>{this.props.tools}</td>
-                <td><NavLink to={this.props.navigate} className = "btn btn-primary">View Repo</NavLink></td>
+                <td><NavLink to={this.props.github} className = "btn btn-primary">View Repo</NavLink></td>
+                <td><NavLink to={this.props.deployed} className="btn btn-primary">View Site</NavLink></td>
                 <td>{this.props.devYear}</td>
               </tr>
             </tbody>
