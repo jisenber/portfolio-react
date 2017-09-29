9ed7e51c5370111b00cf3a290fce9f14ff382c30
jisenber <isenberg.jacob@yahoo.com>
2017-09-29 12:16:22 -0700

    updating portfolio to feature weather app

--- dev/pages/portfolio.jsx
+++ dev/pages/portfolio.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
-import {NavLink} from 'react-router-dom';
-
-
-
 
 export var Portfolio = createReactClass({
   render: function() {
@@ -25,7 +21,7 @@ var Slider = createReactClass({
   },
   //increase and decrease carousel functions
   increase: function(e) {
-    if(this.state.count < 3) {
+    if(this.state.count < 4) {
       this.setState ({
         count: this.state.count + 1
       });
@@ -43,7 +39,7 @@ var Slider = createReactClass({
       });
     } else {
       this.setState ({
-        count: 3
+        count: 4
       });
     }
     e.preventDefault();
@@ -52,9 +48,10 @@ var Slider = createReactClass({
   render: function() {
     return (
       <div>
-        { this.state.count === 1 ? <Slide imgSrc="https://i.imgur.com/4260Qwd.png" title="Space Invaders/Parking Hall of Shame" desc="A platform to shame those who park poorly" tools="Node, Express, jQuery, Mongo, Nunjucks" github="https://github.com/jkwong5/ParkingHOS" deployed="https://parking-hall-of-shame.herokuapp.com" devYear= "2016, 2017 (refactor)"/> : null }
-        { this.state.count === 2 ? <Slide imgSrc="https://i.imgur.com/X6df5yM.png" title="Devolunteer" desc="A platform to connect software developers and non-profits" tools="Mongo, Express, Angular, Node" github="https://github.com/devolunteer/devolunteer" deployed="https://devolunteer.herokuapp.com" devYear="2017"/> : null }
-        { this.state.count === 3 ? <Slide imgSrc="https://i.imgur.com/omgHKYp.png" title="World Capitals Game" desc="A world capitals trivia game (first app I ever built!)" tools="Sweet Vanilla JavaScript" github="https://github.com/jisenber/World_Capitals_Game" deployed="https://jisenber.github.io/World_Capitals_Game" devYear="2015"/> : null }
+        { this.state.count === 1 ? <Slide imgSrc="https://i.imgur.com/763FtEB.png" title="Allweather" desc="A weather forecasting tool for any location or time" tools="React, React-vis, Mongo, Express," github="https://github.com/jisenber/weatherapp-frontend" deployed="https://allweather.herokuapp.com" devYear= "2017"/> : null }
+        { this.state.count === 2 ? <Slide imgSrc="https://i.imgur.com/4260Qwd.png" title="Space Invaders/Parking Hall of Shame" desc="A platform to shame those who park poorly" tools="Node, Express, jQuery, Mongo, Nunjucks" github="https://github.com/jkwong5/ParkingHOS" deployed="https://parking-hall-of-shame.herokuapp.com" devYear= "2016, 2017 (refactor)"/> : null }
+        { this.state.count === 3 ? <Slide imgSrc="https://i.imgur.com/X6df5yM.png" title="Devolunteer" desc="A platform to connect software developers and non-profits" tools="Mongo, Express, Angular, Node" github="https://github.com/devolunteer/devolunteer" deployed="https://devolunteer.herokuapp.com" devYear="2017"/> : null }
+        { this.state.count === 4 ? <Slide imgSrc="https://i.imgur.com/omgHKYp.png" title="World Capitals Game" desc="A world capitals trivia game (first app I ever built!)" tools="Sweet Vanilla JavaScript" github="https://github.com/jisenber/World_Capitals_Game" deployed="https://jisenber.github.io/World_Capitals_Game" devYear="2015"/> : null }
         <button className="btn btn-primary" id="counterNext" onClick={this.increase}>NEXT PROJECT</button>
         <button className="btn btn-primary" id="counterPrev" onClick={this.decrease}>PREVIOUS PROJECT</button>
       </div>
